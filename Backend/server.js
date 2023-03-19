419cce352e6fa902372cc5d7b158ab1f0a1c655f
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-03-20 02:39:11 +0530

    port

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 //   app
 // );
 const server = require("http").createServer(app);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
