f133d3edf747c4a41545f7930dc14aa168780a90
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-03-26 19:53:10 +0530

    changes

--- frontend/src/components/navigation.js
+++ frontend/src/components/navigation.js
@@ -78,14 +78,6 @@ function NavigationBar() {
                 }
                 id="basic-nav-dropdown"
               >
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
                 <NavDropdown.Item classname="navDropdownItem">
                   <button className="logoutBtn" onClick={handleLogout}>
                     Logout
